9a800726c5ee67668c55891babe51a6a1965f59c
kbonga2-appdev <stuwrkr-kbonga2@mx.uillinois.edu>
2023-02-26 19:02:51 +0000

    square functioning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+
  def blank_square_form
    render({ :template => "calcu_templates/square_form.html.erb"})
  end
+
+ def calculate_square
+  @num = params.fetch("input").to_f
+  @square_of_num = @num ** 2
+
+    render({ :template => "calcu_templates/square_results.html.erb"})
+ end
+
 end
