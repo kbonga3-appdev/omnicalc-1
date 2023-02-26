7cf9b33e4c02db85c699c31f3d9159dbc1302fa2
kbonga2-appdev <stuwrkr-kbonga2@mx.uillinois.edu>
2023-02-26 20:46:29 +0000

    random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,18 @@ class ApplicationController < ActionController::Base
     render({ :template => "calcu_templates/square_results.html.erb"})
  end
 
+ def form_random
+  render({ :template => "calcu_templates/random_form.html.erb"})
+end
+
+def calculate_random
+
+  @lower = params.fetch("min_input").to_f
+  @upper = params.fetch("max_input").to_f
+  @result = rand(@lower..@upper)
+
+  render({ :template => "calcu_templates/random_results.html.erb"})
+end
+
+
 end
